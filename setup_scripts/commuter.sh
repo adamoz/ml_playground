35b807ca650d0fbf3fb9fdfd97672b5c82f18375
Adam Kolar <adakolar@gmail.com>
2019-10-02 20:54:15 +0200

    feature: Coloring init

--- setup_scripts/commuter.sh
+++ setup_scripts/commuter.sh
@@ -1,12 +1,12 @@
-echo "$(tput setaf 2)Adding yarn to apt source list.."
+echo "$(tput setaf 2)Adding yarn to apt source list...$(tput init)"
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt update
 sudo apt install yarn
-echo "$(tput setaf 2)Exporting path to yarn binary into the basrc.."
+echo "$(tput setaf 2)Exporting path to yarn binary into the basrc...$(tput init)"
 echo 'export PATH="$(yarn global bin --offline):$PATH"' | sudo tee -a ~/.bashrc
 source ~/.bashrc
-echo "$(tput setaf 2)Installing commuter"
+echo "$(tput setaf 2)Installing commuter...$(tput init)"
 yarn global add @nteract/commuter
 
-echo -e "\n\n$(tput setab 0)$(tput setaf 2)Usage: \nCOMMUTER_LOCAL_STORAGE_BASEDIRECTORY=./ commuter\nCOMMUTER_BUCKET=bb-experiments COMMUTER_STORAGE_BACKEND=s3 commuter"
+echo -e "\n\n$(tput setab 0)$(tput setaf 2)Usage: \nCOMMUTER_LOCAL_STORAGE_BASEDIRECTORY=./ commuter\nCOMMUTER_BUCKET=bb-experiments COMMUTER_STORAGE_BACKEND=s3 commuter$(tput init)"
